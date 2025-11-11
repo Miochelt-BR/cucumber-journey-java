60fe25dce6820c10efe058a04c3525e351809e23
Miochelt-BR <thiago.m.a@outlook.com>
2025-11-11 16:53:14 -0300

    Iniciando projeto de aprendizado BDD com Cucumber em Java: configuração inicial do Maven e primeiros testes

---
+++ src/main/java/org/example/AprenderCucumber.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.it.Quando;
+import io.cucumber.java.pt.Entao;
+
+public class AprenderCucumber {
+   @Dado("que cria arquivo corretamente")
+    public void que_cria_arquivo_corretamente() {
+
+
+    }
+    @Quando("quando executa-lo")
+    public void quando_executa_lo() {
+
+
+    }
+    @Entao("a especificacao deve finalizar com sucesso")
+    public void a_especificacao_deve_finalizar_com_sucesso() {
+
+
+    }
+}
---
+++ src/main/java/org/example/aprender_cucumber.feature
@@ -0,0 +1,7 @@
+# language: pt
+Funcionalidade: aprender cucumber
+
+  Cenário: deve executar a especificação
+    Dado que cria arquivo corretamente
+    Quando quando executa-lo
+    Então a especificacao deve finalizar com sucesso
